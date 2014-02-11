ee972854dc48e512c5fce265e683290311fbdab5
Zack <admin@Admins-Mac.local>
2014-02-11 15:37:53 -0600

    Initial Commit

---
+++ Echoo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Echoo
+//
+//  Created by Admin on 2/11/14.
+//  Copyright (c) 2014 Echoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Echoo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Echoo
+//
+//  Created by Admin on 2/11/14.
+//  Copyright (c) 2014 Echoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
