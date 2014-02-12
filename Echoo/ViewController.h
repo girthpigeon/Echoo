21f8168a3248752a608e109f0770271bd0790122
Zack <admin@Admins-Mac.local>
2014-02-11 20:38:53 -0600

    first attempt at creating a user based of keychains

--- Echoo/ViewController.h
+++ Echoo/ViewController.h
@@ -7,7 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "KeychainItemWrapper.h"
 
 @interface ViewController : UIViewController
+{
+    NSString *loginname;
+    NSString *loginpassword;
+    NSString *dbUrl;
+    NSString *db;
+}
+@property (strong, nonatomic) IBOutlet UITextField *username_textarea;
+@property (strong, nonatomic) IBOutlet UITextField *password_textarea;
+@property (strong, nonatomic) IBOutlet UITextField *password2_textarea;
 
 @end
--- Echoo/ViewController.m
+++ Echoo/ViewController.m
@@ -1,8 +1,8 @@
 //
 //  ViewController.m
-//  Echoo
+//  Audio Upload Demo
 //
-//  Created by Admin on 2/11/14.
+//  Created by Admin on 1/16/14.
 //  Copyright (c) 2014 Echoo. All rights reserved.
 //
 
@@ -14,10 +14,56 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    KeychainItemWrapper* keychain = [[KeychainItemWrapper alloc] initWithIdentifier:@"KeychainTest" accessGroup:nil];
+    
+    if ([keychain objectForKey:(__bridge id)(kSecAttrAccount)]) {
+        // existing value
+    } else {
+        // no existing value
+    int userid = [self createNewUser];
+        NSLog(@"new userid is: %ld", (long)userid);
+    }
+    
+    //NSString *username = self.username_textarea.text;
+    //NSString *password = self.password_textarea.text;
+    //NSString *password2 = self.password2_textarea.text;
+    
+    //if([password2 isEqualToString:password]){
+        //do call  to server
+    //}
+    
+    //if user already has an account remember it
+    
+    //if not then allow user to create a new account or continue annonymously
+    
+    //otherwise create new user
+    
+    
+}
+
+- (int)createNewUser {
+    //NSString *latitude = [[NSString alloc] initWithFormat:@"%g", currentLocation.coordinate.latitude];
+    //NSString *longitude = [[NSString alloc] initWithFormat:@"%g", currentLocation.coordinate.longitude];
+    
+    NSURL *url2 = [NSURL URLWithString:@"http://kwipp.com/echoo/php/register.php"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL: url2];
+    request.HTTPMethod = @"POST";
+    NSString *post = [NSString stringWithFormat:@"&loginname=%@&loginpassword=%@&dbUrl=%@&db=%@", loginname, loginpassword, dbUrl, db];
+    NSData *postData = [post dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
+    //NSString *postLength = [NSString stringWithFormat:@"%d",[postData length]];
+    request.HTTPBody = postData;
+    [NSURLConnection sendAsynchronousRequest: request
+                                       queue: [NSOperationQueue mainQueue]
+                           completionHandler:
+     ^(NSURLResponse *r, NSData *data, NSError *error) {
+         NSLog(@"response: %@", r);
+     }];
+    
+    int *userid = 0;
+    return *userid;
 }
 
 - (void)didReceiveMemoryWarning
